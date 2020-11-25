d72c759bd50c7c1632b231740876593c0ef129bc
davidgallach <davidgallach@gmail.com>
2020-11-25 11:04:10 +0100

    Date of birth added

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-name='David'
-first_surname='Gallach'
-second_surname='Sanchez'
+name = 'David'
+first_surname = 'Gallach'
+second_surname = 'Sanchez'
 
-print('My full name is',name,first_surname,second_surname)
+print('My full name is', name, first_surname, second_surname)
+date_birth = '04/06/1987'
+print('My date of birth is', date_birth)
